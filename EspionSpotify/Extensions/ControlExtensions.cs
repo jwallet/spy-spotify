8ee52a8d007c9fe058bff7e1b66a66d16c5ed0ce
Steve Hayles <stevehayles@gmail.com>
2020-08-31 19:13:59 +0100

    Fix cross-thread control calls
    A number of invalidoperation exceptions were being thrown due to calls not being marshalled back to the main thread

---
+++ EspionSpotify/Extensions/ControlExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EspionSpotify.Extensions
+{
+    public static class ControlExtensions
+    {
+        public static Result GetThreadSafeValue<T, Result>(this T control, Expression<Func<T, Result>> selector)
+            where T : Control
+        {
+            Func<T, Result> func = selector.Compile();
+
+            if (control.InvokeRequired)
+            {
+                return (Result)control.Invoke(new Func<Result>(() => func(control)));
+            }
+            else
+            {
+                return func(control);
+            }
+        }
+    }
+}
