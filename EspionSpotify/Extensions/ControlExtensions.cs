6d066193c460b83cdff29c598e6d748f2b085f73
jwallet <joseouellet@gmail.com>
2020-10-01 21:43:54 -0400

    Safe thread getter/setter

--- EspionSpotify/Extensions/ControlExtensions.cs
+++ EspionSpotify/Extensions/ControlExtensions.cs
@@ -10,18 +10,29 @@ namespace EspionSpotify.Extensions
 {
     public static class ControlExtensions
     {
-        public static Result GetThreadSafeValue<T, Result>(this T control, Expression<Func<T, Result>> selector)
-            where T : Control
+        public static TResult GetPropertyThreadSafe<TControl, TResult>(this TControl control, Func<TControl, TResult> getter)
+            where TControl : Control
         {
-            Func<T, Result> func = selector.Compile();
-
             if (control.InvokeRequired)
             {
-                return (Result)control.Invoke(new Func<Result>(() => func(control)));
+                return (TResult)control.Invoke(getter, control);
             }
             else
             {
-                return func(control);
+                return getter(control);
+            }
+        }
+
+        public static void SetPropertyThreadSafe<TControl>(this TControl control, MethodInvoker setter)
+           where TControl : Control
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(setter);
+            }
+            else
+            {
+                setter();
             }
         }
     }
