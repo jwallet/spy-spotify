3c1764771f21bba73b0f5f378bd22a9072e845b6
Wallet <joseouellet@gmail.com>
2021-08-06 21:25:47 -0400

    Removed foreign languages

--- EspionSpotify/Translations/Languages.cs
+++ EspionSpotify/Translations/Languages.cs
@@ -10,18 +10,8 @@ namespace EspionSpotify.Translations
     {
         public static readonly Dictionary<LanguageType, Type> availableResourcesManager = new Dictionary<LanguageType, Type>
         {
-            { LanguageType.cs, typeof(cs) },
-            { LanguageType.de, typeof(de) },
             { LanguageType.en, typeof(en) },
-            { LanguageType.es, typeof(es) },
             { LanguageType.fr, typeof(fr) },
-            { LanguageType.it, typeof(it) },
-            { LanguageType.ja, typeof(ja) },
-            { LanguageType.nl, typeof(nl) },
-            { LanguageType.pl, typeof(pl) },
-            { LanguageType.pt, typeof(pt) },
-            { LanguageType.ru, typeof(ru) },
-            { LanguageType.tr, typeof(tr) },
         };
 
         public static Type GetResourcesManagerLanguageType(LanguageType? type)
@@ -37,17 +27,7 @@ namespace EspionSpotify.Translations
         internal static readonly Dictionary<LanguageType, string> dropdownListValues = new Dictionary<LanguageType, string>
         {
             { LanguageType.en, "English" },
-            //{ LanguageType.cs, "Čeština" }, // Czech
-            //{ LanguageType.de, "Deutsch" }, // German
-            //{ LanguageType.es, "Española" }, // Spanish
             { LanguageType.fr, "Français" }, // French
-            //{ LanguageType.it, "Italiano" }, // Italian
-            //{ LanguageType.ja, "Nihonjin" }, // Japanese
-            //{ LanguageType.nl, "Nederlands" }, // Dutch
-            //{ LanguageType.pl, "Polskie" }, // Polish
-            //{ LanguageType.pt, "Português" }, // Portuguese
-            //{ LanguageType.ru, "Roshia" }, // Russian
-            //{ LanguageType.tr, "Türk" }, // Turkish
         };
     }
 }
